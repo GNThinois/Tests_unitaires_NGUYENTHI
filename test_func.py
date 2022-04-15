7c6c274f959a52974dd9eda39a74d0746144e800
NGUYEN THI Guillaume <guillaume.nguyenthi@gmail.com>
2022-04-15 13:37:38 +0200

    ajout de tests

--- test_func.py
+++ test_func.py
@@ -1,11 +1,21 @@
 from Module import Person, Wizard, HealthPotion
 
 def test_init():
-    user_test1 = Person("Hero")
-    expected_result = 100
-    assert user_test1.life_points == expected_result
+    test_person = Person("Hero")
+    #user_test2 = Wizard("Wizard")
+    expected_result_1 = 100
+    #expected_result_2 = 80
+    assert test_person.life_points == expected_result_1
+    #assert user_test2.life_points == expected_result_2
 
 def test_name():
-    user_test2 = Person("Test")
+    test_person = Person("Test")
     expected_result = "Test"
-    assert user_test2.name == expected_result
+    assert test_person.name == expected_result
+
+def test_hit():
+    test_person = Person("Test1")
+    test_wizard = Wizard("Test2")
+    expected_result = 85
+    test_wizard.hit(test_person)
+    assert test_person.life_points == expected_result
