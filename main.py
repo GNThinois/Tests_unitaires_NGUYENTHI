f4f6c56ded61c2d9343ec15d94b2039b7c7edadf
NGUYEN THI Guillaume <guillaume.nguyenthi@gmail.com>
2022-04-15 10:31:17 +0200

    commit 1 : init github et test simple

--- main.py
+++ main.py
@@ -4,26 +4,28 @@ import numpy as np
 user_1 = Person("Hero")
 user_2 = Wizard("Wizard")
 
-while user_1.is_dead() == False and user_2.is_dead() == False:
-    random = np.random.randint(2)
-    
-    if random == 1:
-        user_1.hit(user_2)
-    else:
-        user_2.hit(user_1)
+def fight(fighter1, fighter2):
+    while fighter1.is_dead() == False and fighter2.is_dead() == False:
+        random = np.random.randint(2)
 
-    random_health_potion_use = np.random.randint(3)
-        
-    if random_health_potion_use == 0:
-        HealthPotion.was_used_by(user_1)
-    elif random_health_potion_use == 1:
-        HealthPotion.was_used_by(user_2)
-    else:
-        pass
+        if random == 1:
+            fighter1.hit(fighter2)
+        else:
+            fighter2.hit(fighter1)
 
-if user_1.get_life_points() <= 0:
-    print(user_1.name + " wins")
-if user_2.get_life_points() <= 0:
-    print(user_2.name + " wins")
+        random_health_potion_use = np.random.randint(3)
+
+        if random_health_potion_use == 0:
+            HealthPotion.was_used_by(user_1)
+        elif random_health_potion_use == 1:
+            HealthPotion.was_used_by(fighter2)
+        else:
+            pass
+
+    if fighter1.get_life_points() <= 0:
+        print(fighter2.name + " wins")
+    if fighter2.get_life_points() <= 0:
+        print(fighter1.name + " wins")
 
 
+#fight(user_1, user_2)
---
+++ test_func.py
@@ -0,0 +1,10 @@
+import main
+from Module import Person, Wizard, HealthPotion
+import numpy as np
+
+def test_init():
+    user_test1 = Person("Hero")
+    expected_result = 80
+    assert user_test1.life_points == expected_result
+
+print('tests finis.')
