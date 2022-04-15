eb32694fdce03aa81e7dfcf26a9236ff5734c3ed
NGUYEN THI Guillaume <guillaume.nguyenthi@gmail.com>
2022-04-15 14:38:22 +0200

    ajout tests

--- test_func.py
+++ test_func.py
@@ -19,3 +19,36 @@ def test_hit():
     expected_result = 85
     test_wizard.hit(test_person)
     assert test_person.life_points == expected_result
+
+def test_is_dead():
+    test_person = Person("Test 1")
+    test_person.life_points = 0
+    expected_result = True
+    assert test_person.is_dead() == expected_result
+
+def test_gained_life_points():
+    test_person = Person("Test 1")
+    test_person.gained_life_points(10)
+    expected_result = 110
+    assert test_person.life_points == expected_result
+
+def test_get_life_points():
+    test_person = Person("Test 1")
+    expected_result = 100
+    assert test_person.life_points == expected_result
+
+def test_multi_func():
+    test_person = Person("Test1")
+    #100hp
+    test_wizard = Wizard("Test2")
+    #80hp
+    HealthPotion.was_used_by(test_person)
+    #+10
+    assert test_person.get_life_points() == 110
+    test_wizard.hit(test_person)
+    #-15
+    assert test_person.get_life_points() == 95
+    test_person.gained_life_points(10)
+    #+10
+    assert test_person.get_life_points() == 105
+
