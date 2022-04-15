d162eeae6cb64213ba7542a8144c27509bdd196c
NGUYEN THI Guillaume <guillaume.nguyenthi@gmail.com>
2022-04-15 13:14:50 +0200

    test 2

--- test_func.py
+++ test_func.py
@@ -4,3 +4,8 @@ def test_init():
     user_test1 = Person("Hero")
     expected_result = 100
     assert user_test1.life_points == expected_result
+
+def test_name():
+    user_test2 = Person("Test")
+    expected_result = "Test"
+    assert user_test2.name == expected_result
